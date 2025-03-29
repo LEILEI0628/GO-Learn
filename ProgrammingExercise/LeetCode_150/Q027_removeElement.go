43c5cf6ed939731d02d87ace7b737ba80c58abd7
LEILEI0628 <2239080538@qq.com>
2025-03-29 23:42:25 +0800

    150Q27

---
+++ ProgrammingExercise/LeetCode_150/Q027_removeElement.go
@@ -0,0 +1,53 @@
+package main
+
+// 27. 移除元素
+
+func removeElement(nums []int, val int) int {
+	diff := make([]int, 0, len(nums))
+	for _, v := range nums {
+		if v != val {
+			diff = append(diff, v)
+		}
+	}
+	copy(nums[0:], diff[0:])
+	return len(diff)
+}
+
+func removeElement1(nums []int, val int) int {
+	i := 0
+	for j, v := range nums {
+		if nums[j] != val {
+			//if i == j {
+			//	i++
+			//	continue
+			//}
+			nums[i] = v
+			i++
+		}
+	}
+	return i
+}
+
+func removeElementA1(nums []int, val int) int {
+	left := 0
+	for _, v := range nums { // v 即 nums[right]
+		if v != val {
+			nums[left] = v
+			left++
+		}
+	}
+	return left
+}
+
+func removeElementA2(nums []int, val int) int {
+	left, right := 0, len(nums)
+	for left < right {
+		if nums[left] == val {
+			nums[left] = nums[right-1]
+			right--
+		} else {
+			left++
+		}
+	}
+	return left
+}
