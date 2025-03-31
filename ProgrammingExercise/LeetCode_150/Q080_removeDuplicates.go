078f573a8c58a7f9a45918dadf717ef31b3cdf19
LEILEI0628 <2239080538@qq.com>
2025-03-31 23:34:49 +0800

    150Q80

---
+++ ProgrammingExercise/LeetCode_150/Q080_removeDuplicates.go
@@ -0,0 +1,33 @@
+package main
+
+// 80. 删除有序数组中的重复项 II https://leetcode.cn/problems/remove-duplicates-from-sorted-array-ii/description/?envType=study-plan-v2&envId=top-interview-150
+
+func Q080removeDuplicates(nums []int) int {
+	if len(nums) <= 2 {
+		return len(nums)
+	}
+	slow := 2
+	for fast := 2; fast < len(nums); fast++ {
+		if nums[slow-2] != nums[fast] {
+			nums[slow] = nums[fast]
+			slow++
+		}
+	}
+	return slow
+}
+
+func Q080removeDuplicatesA1(nums []int) int {
+	n := len(nums)
+	if n <= 2 {
+		return n
+	}
+	slow, fast := 2, 2
+	for fast < n {
+		if nums[slow-2] != nums[fast] {
+			nums[slow] = nums[fast]
+			slow++
+		}
+		fast++
+	}
+	return slow
+}
