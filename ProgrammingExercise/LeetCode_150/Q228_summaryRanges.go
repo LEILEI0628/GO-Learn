8b601e14939153c7ea2aad619823e4de47ca8882
LEILEI0628 <2239080538@qq.com>
2025-04-03 00:04:50 +0800

    update 150Q228

--- ProgrammingExercise/LeetCode_150/Q228_summaryRanges.go
+++ ProgrammingExercise/LeetCode_150/Q228_summaryRanges.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func summaryRanges(nums []int) []string {
 	if len(nums) == 0 {
@@ -27,4 +30,40 @@ func summaryRanges(nums []int) []string {
 		result = append(result, fmt.Sprintf("%d->%d", nums[i], nums[j-1]))
 	}
 	return result
+}
+
+func summaryRanges1(nums []int) (ans []string) {
+	if len(nums) == 0 {
+		return []string{}
+	}
+	start := 0
+	end := 0
+	for end < len(nums) {
+		end++
+		for (end < len(nums)) && (nums[end]-nums[end-1] == 1) {
+			end++
+		}
+		if start == end-1 {
+			ans = append(ans, fmt.Sprintf("%d", nums[start]))
+		} else {
+			ans = append(ans, fmt.Sprintf("%d->%d", nums[start], nums[end-1]))
+		}
+		start = end
+
+	}
+	return
+}
+
+func summaryRangesA1(nums []int) (ans []string) {
+	for i, n := 0, len(nums); i < n; {
+		left := i
+		for i++; i < n && nums[i-1]+1 == nums[i]; i++ {
+		}
+		s := strconv.Itoa(nums[left])
+		if left < i-1 {
+			s += "->" + strconv.Itoa(nums[i-1])
+		}
+		ans = append(ans, s)
+	}
+	return
 }
