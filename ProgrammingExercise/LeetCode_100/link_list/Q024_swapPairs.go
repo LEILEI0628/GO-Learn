3358f9e3fc56a47ca8d65b920a146b04b6b20a87
LEILEI0628 <2239080538@qq.com>
2025-04-22 01:50:57 +0800

    update list

---
+++ ProgrammingExercise/LeetCode_100/link_list/Q024_swapPairs.go
@@ -0,0 +1,68 @@
+package main
+
+// 24. 两两交换链表中的节点 https://leetcode.cn/problems/swap-nodes-in-pairs/description/?envType=study-plan-v2&envId=top-100-liked
+// 给你一个链表，两两交换其中相邻的节点，并返回交换后链表的头节点。你必须在不修改节点内部的值的情况下完成本题（即，只能进行节点交换）。
+
+func swapPairs(node *ListNode) *ListNode {
+	if node == nil || node.Next == nil {
+		return node
+	}
+	result := node.Next
+	p := swapPairs(node.Next.Next)
+	node.Next.Next = node
+	node.Next = p
+
+	return result
+}
+
+// 数组
+func swapPairs1(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	nodeList := make([]*ListNode, 0)
+	p := head
+	for p != nil {
+		nodeList = append(nodeList, p)
+		p = p.Next
+	}
+	last := len(nodeList)
+	last -= last % 2
+	for i := last - 1; i >= 0; i -= 2 {
+		var temp *ListNode          // 初始值为nil，若为最后两个节点则temp为nil
+		if i+1 == len(nodeList)-1 { // 节点数为奇数的情况
+			temp = nodeList[i+1]
+		} else if i+2 < len(nodeList) { // 非最后两个节点的情况
+			temp = nodeList[i+2]
+		}
+		nodeList[i].Next = nodeList[i-1]
+		nodeList[i-1].Next = temp
+	}
+	return nodeList[1]
+}
+
+func swapPairs2(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	result := head.Next
+	p := head
+	for p != nil {
+		if p.Next.Next != nil && p.Next.Next.Next == nil { // 奇数最后三个节点的情况
+			temp := p.Next.Next
+			p.Next.Next = p
+			p.Next = temp
+			break
+		} else if p.Next.Next == nil { // 偶数最后两个节点的情况
+			p.Next.Next = p
+			p.Next = nil
+			break
+		}
+		next := p.Next.Next      // 下一个p节点的位置
+		temp := p.Next.Next.Next // 当前节点指向的更改后的下一个节点的位置
+		p.Next.Next = p
+		p.Next = temp
+		p = next
+	}
+	return result
+}
---
+++ ProgrammingExercise/LeetCode_100/link_list/Q25_reverseKGroup.go
@@ -0,0 +1,62 @@
+package main
+
+// 25. K 个一组翻转链表 https://leetcode.cn/problems/reverse-nodes-in-k-group/description/?envType=study-plan-v2&envId=top-100-liked
+
+func reverseKGroup(head *ListNode, k int) *ListNode {
+	nodeList := make([]*ListNode, 0)
+	p := head
+	for p != nil {
+		nodeList = append(nodeList, p)
+		p = p.Next
+	}
+	head = &ListNode{}
+	q := head
+	for i := 0; i < len(nodeList); i += k {
+		if i+k-1 >= len(nodeList) { // 最后的的节点不够交换
+			q.Next = nodeList[i]
+			break
+		}
+		for j := k - 1; j >= 0; j-- {
+			q.Next = nodeList[i+j]
+			q = q.Next
+		}
+	}
+	return head.Next
+}
+
+// 方法一：模拟
+// 思路与算法
+// 本题的目标非常清晰易懂，不涉及复杂的算法，但是实现过程中需要考虑的细节比较多，容易写出冗长的代码。主要考查面试者设计的能力。
+func reverseKGroup1(head *ListNode, k int) *ListNode {
+	hair := &ListNode{Next: head}
+	pre := hair
+
+	for head != nil {
+		tail := pre
+		for i := 0; i < k; i++ {
+			tail = tail.Next
+			if tail == nil {
+				return hair.Next
+			}
+		}
+		nex := tail.Next
+		head, tail = myReverse(head, tail)
+		pre.Next = head
+		tail.Next = nex
+		pre = tail
+		head = tail.Next
+	}
+	return hair.Next
+}
+
+func myReverse(head, tail *ListNode) (*ListNode, *ListNode) {
+	prev := tail.Next
+	p := head
+	for prev != tail {
+		nex := p.Next
+		p.Next = prev
+		prev = p
+		p = nex
+	}
+	return tail, head
+}
