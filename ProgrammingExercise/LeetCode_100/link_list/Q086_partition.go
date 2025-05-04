ff6f4d3979a92b8b11578ecb37fd3f8197a87267
LEILEI0628 <2239080538@qq.com>
2025-05-05 00:30:38 +0800

    100Q86

--- ProgrammingExercise/LeetCode_100/link_list/Q086_partition.go
+++ ProgrammingExercise/LeetCode_100/link_list/Q086_partition.go
@@ -4,10 +4,10 @@ package main
 // 给你一个链表的头节点 head 和一个特定值 x ，请你对链表进行分隔，使得所有 小于 x 的节点都出现在 大于或等于 x 的节点之前。
 // 你应当 保留 两个分区中每个节点的初始相对位置。
 func partition(head *ListNode, x int) *ListNode {
-	left := ListNode{}
-	p := &left
-	right := ListNode{}
-	q := &right
+	left := &ListNode{}
+	p := left
+	right := &ListNode{}
+	q := right
 	for head != nil { // 对链表遍历
 		if head.Val < x { // 小于x的挂到左边
 			p.Next = head
