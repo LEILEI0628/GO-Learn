da0cf89d321643789364c89acd5bf9e11ca32d8e
LEILEI0628 <2239080538@qq.com>
2025-05-10 14:28:04 +0800

    add UncacheChannel

--- syntax/part11_channel/alternate_printing_test.go
+++ syntax/part11_channel/alternate_printing_test.go
@@ -17,7 +17,7 @@ func TestAlternatePrinting(t *testing.T) {
 		defer wg.Done()
 		for i := 1; i < 6; i++ {
 			<-chanA // 等待通道A的信号
-			fmt.Printf("A: %d\n", i)
+			fmt.Printf("A:%d ", i)
 			chanB <- struct{}{} // 向通道B发送信号
 		}
 	}()
@@ -25,11 +25,12 @@ func TestAlternatePrinting(t *testing.T) {
 		defer wg.Done()
 		for i := 0; i < 5; i++ {
 			<-chanB // 等待通道B的信号
-			fmt.Printf("B: %c\n", 'A'+i)
+			fmt.Printf("B:%c ", 'A'+i)
 			chanA <- struct{}{} // 向通道A发送信号
 		}
 	}()
-	wg.Wait() // 阻塞主线程，直到所有子协程完成
+	wg.Wait()                      // 阻塞主线程，直到所有子协程完成
+	fmt.Printf("\n%#v\n", <-chanA) // 此时chanA内还有数据
 	close(chanA)
 	close(chanB)
 }
@@ -44,7 +45,7 @@ func TestAlternatePrintingUncacheV1(t *testing.T) {
 		defer wg.Done()
 		for i := 0; i < 5; i++ {
 			<-chanA // 等待通道A的信号
-			fmt.Printf("A: %d\n", i+1)
+			fmt.Printf("A:%d ", i+1)
 			chanB <- struct{}{} // 向通道B发送信号
 
 		}
@@ -53,7 +54,7 @@ func TestAlternatePrintingUncacheV1(t *testing.T) {
 		defer wg.Done()
 		for i := 0; i < 5; i++ {
 			<-chanB // 等待通道B的信号
-			fmt.Printf("B: %c\n", 'A'+i)
+			fmt.Printf("B:%c ", 'A'+i)
 			if i != 4 { // 此时chanA已经不会再被读取了，程序会阻塞住这里
 				chanA <- struct{}{} // 向通道A发送信号
 			}
@@ -74,7 +75,7 @@ func TestAlternatePrintingUncacheV2(t *testing.T) {
 	go func() { // 协程A：打印数字
 		defer wg.Done()
 		for i := 0; i < 5; i++ {
-			fmt.Printf("A: %d\n", i+1)
+			fmt.Printf("A:%d ", i+1)
 			// 不进行初始化，直接由A开始发送，严格保证AB的发送和接收次数相同
 			chanB <- struct{}{} // 向通道B发送信号
 			<-chanA             // 等待通道A的信号
@@ -84,7 +85,7 @@ func TestAlternatePrintingUncacheV2(t *testing.T) {
 		defer wg.Done()
 		for i := 0; i < 5; i++ {
 			<-chanB // 等待通道B的信号
-			fmt.Printf("B: %c\n", 'A'+i)
+			fmt.Printf("B:%c ", 'A'+i)
 			chanA <- struct{}{} // 向通道A发送信号
 		}
 	}()
